fad4870552acc43bdf9d7c5c4c0792fe17bd7067
evan <xinhui.dai@synapsor.com>
2025-03-25 10:31:33 +0800

    add bluetoothKit

---
+++ CHBluetoothKit/CHBluetoothKit.h
@@ -0,0 +1,18 @@
+//
+//  CHBluetoothKit.h
+//  CHBluetoothKit
+//
+//  Created by Evan Dai on 2025/3/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CHBluetoothKit.
+FOUNDATION_EXPORT double CHBluetoothKitVersionNumber;
+
+//! Project version string for CHBluetoothKit.
+FOUNDATION_EXPORT const unsigned char CHBluetoothKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHBluetoothKit/PublicHeader.h>
+
+
